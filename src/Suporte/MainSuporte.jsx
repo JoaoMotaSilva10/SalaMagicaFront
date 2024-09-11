5c3baff1643ad19a10c9025b9dfce9433b68f41d
gabrielbarbosapessoa <gabrielbarbosapessoa2@gmail.com>
2024-09-11 16:43:09 -0300

    deixei site estilizado roxo

--- src/Suporte/MainSuporte.jsx
+++ src/Suporte/MainSuporte.jsx
@@ -9,18 +9,18 @@ const MainSuporte = () => {
   const data = [
     { id: 1, nome: 'João Silva', rm: '12345', tipoReclamacao: 'Produto defeituoso', reclamacao: 'O produto chegou com defeito e não funciona.' },
     { id: 2, nome: 'Maria Oliveira', rm: '67890', tipoReclamacao: 'Entrega atrasada', reclamacao: 'O produto foi entregue com uma semana de atraso.' },
-    { id: 1, nome: 'João Silva', rm: '12345', tipoReclamacao: 'Produto defeituoso', reclamacao: 'O produto chegou com defeito e não funciona.' },
-    { id: 2, nome: 'Maria Oliveira', rm: '67890', tipoReclamacao: 'Entrega atrasada', reclamacao: 'O produto foi entregue com uma semana de atraso.' },
-    { id: 1, nome: 'João Silva', rm: '12345', tipoReclamacao: 'Produto defeituoso', reclamacao: 'O produto chegou com defeito e não funciona.' },
-    { id: 2, nome: 'Maria Oliveira', rm: '67890', tipoReclamacao: 'Entrega atrasada', reclamacao: 'O produto foi entregue com uma semana de atraso.' },
-    { id: 1, nome: 'João Silva', rm: '12345', tipoReclamacao: 'Produto defeituoso', reclamacao: 'O produto chegou com defeito e não funciona.' },
-    { id: 2, nome: 'Maria Oliveira', rm: '67890', tipoReclamacao: 'Entrega atrasada', reclamacao: 'O produto foi entregue com uma semana de atraso.' },
-    { id: 1, nome: 'João Silva', rm: '12345', tipoReclamacao: 'Produto defeituoso', reclamacao: 'O produto chegou com defeito e não funciona.' },
-    { id: 2, nome: 'Maria Oliveira', rm: '67890', tipoReclamacao: 'Entrega atrasada', reclamacao: 'O produto foi entregue com uma semana de atraso.' },
-    { id: 1, nome: 'João Silva', rm: '12345', tipoReclamacao: 'Produto defeituoso', reclamacao: 'O produto chegou com defeito e não funciona.' },
-    { id: 2, nome: 'Maria Oliveira', rm: '67890', tipoReclamacao: 'Entrega atrasada', reclamacao: 'O produto foi entregue com uma semana de atraso.' },
-    { id: 1, nome: 'João Silva', rm: '12345', tipoReclamacao: 'Produto defeituoso', reclamacao: 'O produto chegou com defeito e não funciona.' },
-    { id: 2, nome: 'Maria Oliveira', rm: '67890', tipoReclamacao: 'Entrega atrasada', reclamacao: 'O produto foi entregue com uma semana de atraso.' },
+    { id: 3, nome: 'João Silva', rm: '12345', tipoReclamacao: 'Produto defeituoso', reclamacao: 'O produto chegou com defeito e não funciona.' },
+    { id: 4, nome: 'Maria Oliveira', rm: '67890', tipoReclamacao: 'Entrega atrasada', reclamacao: 'O produto foi entregue com uma semana de atraso.' },
+    { id: 5, nome: 'João Silva', rm: '12345', tipoReclamacao: 'Produto defeituoso', reclamacao: 'O produto chegou com defeito e não funciona.' },
+    { id: 6, nome: 'Maria Oliveira', rm: '67890', tipoReclamacao: 'Entrega atrasada', reclamacao: 'O produto foi entregue com uma semana de atraso.' },
+    { id: 7, nome: 'João Silva', rm: '12345', tipoReclamacao: 'Produto defeituoso', reclamacao: 'O produto chegou com defeito e não funciona.' },
+    { id: 8, nome: 'Maria Oliveira', rm: '67890', tipoReclamacao: 'Entrega atrasada', reclamacao: 'O produto foi entregue com uma semana de atraso.' },
+    { id: 9, nome: 'João Silva', rm: '12345', tipoReclamacao: 'Produto defeituoso', reclamacao: 'O produto chegou com defeito e não funciona.' },
+    { id: 10, nome: 'Maria Oliveira', rm: '67890', tipoReclamacao: 'Entrega atrasada', reclamacao: 'O produto foi entregue com uma semana de atraso.' },
+    { id: 11, nome: 'João Silva', rm: '12345', tipoReclamacao: 'Produto defeituoso', reclamacao: 'O produto chegou com defeito e não funciona.' },
+    { id: 12, nome: 'Maria Oliveira', rm: '67890', tipoReclamacao: 'Entrega atrasada', reclamacao: 'O produto foi entregue com uma semana de atraso.' },
+    { id: 13, nome: 'João Silva', rm: '12345', tipoReclamacao: 'Produto defeituoso', reclamacao: 'O produto chegou com defeito e não funciona.' },
+    { id: 14, nome: 'Maria Oliveira', rm: '67890', tipoReclamacao: 'Entrega atrasada', reclamacao: 'O produto foi entregue com uma semana de atraso.' },
 
   ];
 
@@ -30,7 +30,8 @@ const MainSuporte = () => {
 
   return (
     <div className="container">
-        <h1 className="hora">Hora de responder as Reclamações!</h1>
+        <h1 className="hora">Hora de responder as</h1>
+        <h1 className="rec">Reclamações!</h1>
         <div className="table-wrapper">
       <table className="support-table">
         <thead>
